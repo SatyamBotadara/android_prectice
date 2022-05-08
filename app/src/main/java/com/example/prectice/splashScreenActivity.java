012aeafad940601fa06744600b9f72cdca541382
satyam <satyambotadara99m@gmail.com>
2022-05-08 07:28:51 +0530

    practice

--- app/src/main/java/com/example/prectice/splashScreenActivity.java
+++ app/src/main/java/com/example/prectice/splashScreenActivity.java
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.example.prectice.databinding.ActivityLoginBinding;
 import com.example.prectice.databinding.ActivitySplashScreenBinding;
 import com.example.prectice.login_registation_app.login_activity;
+
 
 public class splashScreenActivity extends AppCompatActivity {
     ActivitySplashScreenBinding binding;
@@ -17,21 +16,29 @@ public class splashScreenActivity extends AppCompatActivity {
         binding=ActivitySplashScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run(){
-
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Intent i=new Intent(splashScreenActivity.this,login_activity.class);
-                startActivity(i);
-                finish();
-            }
-        }).start();
-
+        Thread t=new Thread(new splase());
+        t.start();
     }
+
+
+    class splase implements  Runnable{
+
+        @Override
+        public void run() {
+            try
+            {
+                Thread.sleep(5000);
+                Intent i=new Intent(splashScreenActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
+
+
