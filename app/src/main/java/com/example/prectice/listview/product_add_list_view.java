5eb7982fb5fec2d35ca4dddf1813e26692f1cb58
satyam <satyambotadara99m@gmail.com>
2022-06-28 12:57:52 +0530

    satyam

---
+++ app/src/main/java/com/example/prectice/listview/product_add_list_view.java
@@ -0,0 +1,17 @@
+package com.example.prectice.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prectice.R;
+
+public class product_add_list_view extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_add_list_view);
+    }
+}
